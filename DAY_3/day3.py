28932ec3b10d1bc1338da4331e393468ffab5967
Sandeep M Waghmare <sandeepwaghmare1999@gmail.com>
2021-06-14 13:36:58 +0530

    Day3

---
+++ DAY_3/day3.py
@@ -0,0 +1,61 @@
+#Operator
+#round()   int()    //
+# /     
+num1 = 15
+num2 = 2
+# print(num1 // num2) # value nearest to the whole number
+# print(num1 / num2)
+# print(num1**num2)
+
+#assignment operator
+name = "harry"
+
+num1 += 3     #num1 = num1 + 3
+num1 -= 3     #num1 = num1 - 3
+num1 *= 3     #num1 = num1 * 3
+# print(num1)
+
+# string
+name = "I love Python"
+print(len(name)) # from 1
+str2 = "East or West India is the best"
+# left---right (0)
+# right---left (-1)
+# print(len(str2))
+# print(str2[0])  #E
+# print(str2[1])  #a
+# print(str2[5])  # 
+# print(str2[len(str2) -1])  # t
+# # range(1,10,1)
+
+
+# quote = "All our dreams can come true, if we have the courage to pursue them"
+
+#var_name[start:end]
+# print(quote[0:len(quote)-1])
+# print(quote[len(quote)-1:0])
+# print(quote[::-1])
+
+
+#palindrome
+# str1=input("Enter string : ")
+# if str1==str1[::-1]:
+#     print("String is palindrome")
+# else:
+#     print("String is not palindrome")
+
+# (condition)?print1:print2   # ternary operator
+
+quote = "All our dreams can come true, if we have the courage to pursue them"
+
+
+print("All our dreams can come true" in  quote)  # true
+
+fruit = "Apple"
+for i in  fruit:
+    print(i)
+
+
+# git desktop
+
+
