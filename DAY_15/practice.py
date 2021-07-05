d016cd0728ff7855af532d6adff0a5deea4b69ae
Sandeep M Waghmare <sandeepwaghmare1999@gmail.com>
2021-07-06 00:06:42 +0530

    Create practice.py

---
+++ DAY_15/practice.py
@@ -0,0 +1,58 @@
+lst = [(206,"Sandeep","Mumabi"),(208,"John","Pune")]
+print(lst[0][2])
+print(lst[1][1])
+#My Name:
+#["Sandeep","John"]
+#Id
+#[206,208]
+#city
+#["Mumabi","pune"]
+
+'''
+Create a new list taking specific elements from a tuple and convert a string value to integer
+student_data  = [('Alberto Franco','15/05/2002','35kg'), ('Gino Mcneill','17/05/2002','37kg'), ('Ryan Parkes','16/02/1999', '39kg'), ('Eesha Hinton','25/09/1998', '35kg')]
+#Names:
+[]
+#DOB:
+[]
+#Weight:
+[]
+
+Q2:Convert all the characters in uppercase and lowercase and eliminate duplicate letters from a sequence
+
+chrars = {'a', 'b', 'E', 'f', 'a', 'i', 'o', 'U', 'a'}
+
+def 
+
+char_new = {(a,A),(b,B)}
+
+
+
+
+Q3: Using map() function, first return a new list with absolute values of existing list. 
+Then for ans_1, find the total sum of the new list's elements.
+
+lst=[99.3890,-3.5, 5, -0.7123, -9, -0.003]
+
+Q4:
+
+l1 = [1,2,5,9,7]
+l2 = [8,9,4,0,1]
+
+op_lst = [(9,-7),(11,,-7)]
+
+'''
+
+l1 = [1,2,5,9,7]
+l2 = [8,9,4,0,1]
+print(list(map(lambda x,y :( (x+y),(x-y)), l1,l2)))
+
+'''
+wrapper():
+
+
+@wrapper
+
+
+
+'''
